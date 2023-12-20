eda4130b022d0de48d33d5a4832e35d5f0b62295
wyq <489264018@qq.com>
2023-12-20 15:37:08 +0800

    init go server

--- main/main.go
+++ main/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main()  {
-	fmt.Println("hello world")
+func main() {
+	fmt.Print(888)
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		fmt.Println("received")
+		c.String(http.StatusOK, "hello World!")
+	})
+
+	r.Run(":7979")
 }
